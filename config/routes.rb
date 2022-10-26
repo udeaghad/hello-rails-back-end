bb8ad388b6a42b42de8f7c773f078f362080ef37
udeaghad <dozie.udeagha@gmail.com>
2022-10-26 16:51:16 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'messages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+    end
+  end
 end
