22a77d6819069e1972fdd13c5dbfdd437323e7ee
udeaghad <dozie.udeagha@gmail.com>
2022-10-26 16:49:37 +0100

    Update schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+msg = Message.create([
+  { body: 'Hello World!' },
+  { body: 'Hello Rails!' },
+  { body: 'Hello React!' },
+  { body: 'Hello Redux!' },
+  { body: 'Hello React-Redux!' }
+])
+
+puts "Created #{msg.count} messages"
